d9fb7595f6c1c731999cd5112f2053e891ecee49
iglidraci <iglidraci98@gmail.com>
2022-03-21 22:44:07 +0100

    tail recursion optimization fibonacci function

--- Week2-Recursion/Fibonacci.java
+++ Week2-Recursion/Fibonacci.java
@@ -2,6 +2,7 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		int nr = Integer.valueOf(args[0]);
 		System.out.println("Fibonacci of " + nr + " = " + goodFibonacci(nr));
+		System.out.println("Fibonacci of " + nr + " = " + fibTL(nr));
 	}
 	public static long badFibonacci(int nr) {
 		if (nr == 0) return 0;
@@ -21,5 +22,16 @@ public class Fibonacci {
 		long result = fib(nr-1, memo) + fib(nr-2, memo);
 		memo[nr] = result;
 		return result;
+	}
+	public static long fibTL(int nr) {
+		// tail-recursion optimization
+		return fibTL(nr, 0, 1);
+	}
+	private static long fibTL (int nr, long a, long b) {
+		if (nr == 0)
+			return a;
+		else if (nr == 1)
+			return b;
+		else return fibTL(nr-1, b, a+b);
 	}
 }
