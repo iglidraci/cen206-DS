7c3246bd346bb2d3a4959a41f0101292e66742d4
igli <iglidraci98@gmail.com>
2023-05-09 00:15:00 +0200

    exercises

---
+++ 2022-2023/week10_hash_tables/Exercises.java
@@ -0,0 +1,29 @@
+public class Exercises {
+    public static void main(String[] args) {
+
+    }
+
+    boolean containsDuplicate(int[] nums) {
+        /*
+        * Given an array of integers, return true if any value appears at least twice
+        * and return false if every element is unique
+        * Solve it in liner time*/
+        return false;
+    }
+
+    boolean containsDuplicate2(int[] nums, int k) {
+        /*
+        * Given an integer array nums and an integer k,
+        * return true if there are two distinct indices i and j in the array
+        * such that nums[i] == nums[j] and abs(i - j) <= k.
+        * Solve it in linear time.
+        * Example 1:
+        * Input: nums = [1, 2, 3, 1], k = 3
+        * Output: true (3-0 <= 3)
+        * ----------
+        * Example 2:
+        * Input: nums = [1, 2, 3, 1, 2, 3], k = 2
+        * Output: false*/
+        return false;
+    }
+}
